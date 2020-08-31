50329121c1d214eb55a2a8b40fcf94e42be61ecb
SKYLYton <35685348+SKYLYton@users.noreply.github.com>
2020-08-31 14:22:56 +0300

    JavaFX 1

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -15,5 +15,34 @@ import java.util.Date;
 import java.util.ResourceBundle;
 
 public class Controller {
+    @FXML
+    private TextArea textArea;
+    @FXML
+    private TextField textField;
 
+    public void onClickBtn(ActionEvent actionEvent) {
+        printMessage("Яков", textField.getText());
+    }
+
+    private void printMessage(String name, String message){
+        if(!message.isEmpty() && !name.isEmpty()){
+
+            textArea.appendText(name + ": " +textField.getText() + " " + getTime() + "\n");
+            textField.setText("");
+            textField.requestFocus();
+
+        }
+    }
+
+    private String getTime(){
+        Date date = new Date();
+        SimpleDateFormat formatForDateNow = new SimpleDateFormat("HH:mm dd.MM.yyyy");
+        return "(" + formatForDateNow.format(date) + ")";
+    }
+
+    public void onKeyPrsd(KeyEvent keyEvent) {
+        if(keyEvent.getCode().equals(KeyCode.ENTER)){
+            printMessage("Яков", textField.getText());
+        }
+    }
 }
